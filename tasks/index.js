6c12dc37c7ed8ac491681aed4ba26f05b1c48482
danyn <danyn.com@gmail.com>
2019-05-04 10:54:08 -0700

    Stuff

--- tasks/index.js
+++ tasks/index.js
@@ -1,17 +1,34 @@
 const gulp = require('gulp');
+const replace = require('gulp-replace');
+var concat = require('gulp-concat');
 
 function watchCSS() {
-  const CSSsrcWatch  = './site/src/css/**/*.css';
-  const CSSsrc  = './site/src/css/style.css';
-  const CSSdest = './site/dist/css/';
+  const CSSsrcWatch  = ['./site/src/**/*.css', './site/src/**/*.css.liquid' ];
+  const CSSsrc  = './site/src/css/style.css.liquid';
+  const CSSdest = './site/dist/assets/';
   const postcss = require('gulp-postcss');
   
-  function CSScompiler() {
-    return gulp.src(CSSsrc)
-          .pipe( postcss([ require('postcss-import'), require('autoprefixer'), require('postcss-nested')]) )
-          .pipe( gulp.dest(CSSdest) );
+  function CSScompiler( CSSfile, CSScompiled ) {
+    return gulp.src(CSSfile)
+          .pipe( postcss([ require('postcss-import'), require('autoprefixer'), require('postcss-nested'), require('postcss-simple-vars')]) )
+          .pipe(replace("'{{'", '{{'))
+          .pipe(replace("'}}'", '}}'))
+          .pipe( gulp.dest( CSScompiled ) );
   }
-  gulp.watch(CSSsrcWatch, CSScompiler);
+
+  if (process.env.NODE_ENV === 'development') {
+  gulp.watch(CSSsrcWatch, function CSSCompilation(done) {
+    CSScompiler(CSSsrc, CSSdest);
+    CSScompiler('./site/src/shopify/templates/product/product.css.liquid', CSSdest);
+    done();
+  });
+  }else{
+
+    return  gulp.src('./site/dist/assets/*.css.liquid')
+           .pipe(concat('all.css'))
+           .pipe(gulp.dest('./site/dist/snippets'));
+
+  }
 }
 
 function watchJS() {
@@ -20,10 +37,14 @@ function watchJS() {
   gulp.watch(JSsrc, JScompiler);
 }
 
+const watchDev = gulp.parallel( watchCSS );
+// const watchDev = gulp.parallel( watchJS, watchCSS );
 
-const watch = gulp.parallel( watchJS, watchCSS );
 
 
+exports.watchDev = watchDev;
 
-exports.watch = watch;
+//  ignore an error put as final stip in chain
+//  .on('error', function (err) { console.log(err.toString()); this.emit('end');})
 
+// conditional upon NODE_ENV
