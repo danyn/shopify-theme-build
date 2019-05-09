1ae15584a3fa34e88d81aec165b7da7fbfb470b8
danyn <danyn.com@gmail.com>
2019-05-09 10:39:09 -0700

    move config into functions for settings

--- tasks/webpack-config.js
+++ tasks/webpack-config.js
@@ -4,45 +4,57 @@
 const path = require('path');
 const webpack = require('webpack');
 
+const JsPaths = {
+  src_resolve: '../site',
+  src_file: './src/js/chinook.js',
+  out_resolve: '../bulk/js/theme/',
+  out_file: './chinook.js'
+  }
 
-let config = {
-    mode: 'development',
-    entry: './src/js/chinook.js',
-    output: {
-        filename: './chinook.js',
-        path: path.resolve(__dirname, '../bulk/js/theme/')
-    },
-    context: path.resolve(__dirname, '../site'),
-    module: {
-        rules: [
-            {
-              test: /\.m?js$/,
-              exclude: /(node_modules|bower_components)/,
-              use: {
-                loader: 'babel-loader',
-                options: {
-                  presets: ['@babel/preset-env']
+
+// src_resolve, src_file, out_resolve, out_file
+
+
+function webpackConfigObject (config_object) {
+  const {src_resolve, src_file, out_resolve, out_file} = config_object;
+  return {
+      context: path.resolve(__dirname, src_resolve),
+      entry: src_file,
+      mode: 'development',
+      output: {
+          path: path.resolve(__dirname, out_resolve),
+          filename: out_file
+      },
+      module: {
+          rules: [
+              {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                  loader: 'babel-loader',
+                  options: {
+                    presets: ['@babel/preset-env']
+                  }
                 }
               }
-            }
-          ]
-    },
-    plugins: [
-        new webpack.ProvidePlugin({
-           $: 'jquery'
-         })
-    ]
+            ]
+      }
+  }
 }
 
+
+
 function jsCompiler() {
+// JsPaths.src.resolve, JsPaths.src.file, JsPaths.out.resolve, JsPaths.out.file
+  const config = webpackConfigObject(JsPaths);  
 
-    return new Promise(resolve => webpack(config, (err, stats) => {
+  return new Promise(resolve => webpack(config, (err, stats) => {
 
-        if (err) console.log('Webpack: ', err)
+      if (err) console.log('Webpack: ', err)
 
-        console.log(stats.toString({ /* stats options */ }))
+      console.log(stats.toString({ /* stats options */ }))
 
-        resolve()
+      resolve()
     }))
 }
 
